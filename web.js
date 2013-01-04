ba26878ad645033ba8c4173c2e8c19e0761a576f
John Bender <john@generalui.com>
2013-01-04 12:13:29 -0800

    print out mongo connection info

--- web.js
+++ web.js
@@ -2,8 +2,14 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var mongo = require('mongoskin');
+var mongodb = require('mongodb');   // just to get the ObjectID type, use the skin for everything else
+var ObjectID = require('mongodb').ObjectID;  // Get the objectID type
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send( process.env.MONGOHQ_URL )
+
 });
 
 var port = process.env.PORT || 5000;
